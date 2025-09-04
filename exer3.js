12d05210e1cfff40b25336ca47183f5301c1cc39
yasminignacio <ignacio.yasmin@escola.pr.gov.br>
2025-09-04 09:44:45 -0300

    Add files via upload

---
+++ exer3.js
@@ -0,0 +1,10 @@
+// 1. Defina uma variável para a idade
+const idade = 20;
+    // 2. Crie a estrutura condicional `if...else`
+    if (idade >= 18) {
+        // Este bloco de código é executado se a condição (idade >= 18) for verdadeira
+        console.log("Você pode comprar bebida alcoólica. Aproveite com responsabilidade!");
+    } else {
+        // Este bloco é executado se a condição for falsa
+        console.log("Desculpe, você não tem idade para comprar bebidas alcoólocas.");
+    }
